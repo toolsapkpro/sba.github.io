6fac7d1c15a6b0d07e766c711c81ee3c40c79c8e
toolsapkpro <145754947+toolsapkpro@users.noreply.github.com>
2024-01-04 20:02:09 -0800

    Add files via upload

---
+++ d.js
@@ -0,0 +1,8 @@
+function myImg() {
+    alert("Ready to buy Password now?")
+}
+function pasuser(form) {
+    if
+     (form.pass.value=="111") { location="result.htm" }
+    else { alert("Wrong Password, Buy Now!") }
+}
